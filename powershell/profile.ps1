e5164f471e3db10507ce738576564069f84f5b58
eboboeing-747 <silinskiymihail@gmail.com>
2026-04-24 19:56:54 +0300

    add some alias to powershell

--- powershell/profile.ps1
+++ powershell/profile.ps1
@@ -3,4 +3,12 @@
 
 Invoke-Expression (&starship init powershell)
 $ENV:STARSHIP_CONFIG = "$HOME\.config\starship\starship.toml"
+$XDG_CONFIG_HOME = "$HOME\.config"
+# $DOTNET_HOME = "C:\Program Files\dotnet"
+$dev = "D:\\dev"
+
+New-Alias -Name pgctl -Value pg_ctl
+
+function tree { lsd --tree @args }
+
 clear
