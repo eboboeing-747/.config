c6d29348170c916efddf683833b7c9a4e3f7ee14
eboboeing-747 <silinskiymihail@gmail.com>
2026-07-31 01:02:25 +0300

    multiple changes
    add AcceptSuggestion keybind
    change ls alias to lsd

--- powershell/profile.ps1
+++ powershell/profile.ps1
@@ -1,13 +1,19 @@
 # add this line to $PROFILE
 # . "~\.config\powershell\profile.ps1"
 
+Set-PSReadLineKeyHandler -Chord "Ctrl+y" -Function AcceptSuggestion
+
 Invoke-Expression (&starship init powershell)
 $ENV:STARSHIP_CONFIG = "$HOME\.config\starship\starship.toml"
+
 $XDG_CONFIG_HOME = "$HOME\.config"
+
 # $DOTNET_HOME = "C:\Program Files\dotnet"
+
 $dev = "D:\\dev"
 
 New-Alias -Name pgctl -Value pg_ctl
+Set-Alias -Name ls -Value lsd # only worsk on powershell versions above 6
 
 function tree { lsd --tree @args }
 
